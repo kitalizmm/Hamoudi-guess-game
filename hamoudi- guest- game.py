ad16568a2729206de8c62837b764ac83c9b3d0aa
kitalizmm <kitalizmm@gmail.com>
2025-05-17 16:19:30 +0300

    Add files via upload

---
+++ hamoudi- guest- game.py
@@ -0,0 +1,121 @@
+10:59:27 I initDevice 8.4
+10:59:27 D initDevice MANUFACTURER:realme DEVICE:RE5C91L1 MODEL:RMX3890 BRAND:realme BOARD:bengal HARDWARE:qcom SDK_INT:34
+10:59:27 E CameraId: 0 CameraRawSizes: [4000x3000]
+10:59:27 E CameraId: 1 CameraRawSizes: [3264x2448]
+10:59:27 E GetFilteredCameraIDs 0,1
+10:59:27 I Back  Main Lens Id 0
+10:59:27 I Front Main Lens Id 1
+10:59:27 I AGC initMenuValue
+10:59:28 I AGC initInfoPage
+10:59:28 I Device Interface CameraID:0 9 => raven => flame
+10:59:28 I AGC experimentalKeys 0
+10:59:28 E getZslFrameCount 7
+10:59:28 E getZslFrameCount 9
+10:59:28 E getZslFrameCount 12
+10:59:28 E getZslFrameCount 20
+10:59:28 D getPortraitZoomValue 1.5 1.5
+10:59:28 I setDeveloperSettingsNonPixel
+10:59:28 I StreamConfigurationMap: 9; SDK: 34
+10:59:28 I loadLibrary gcastartup
+10:59:28 W load gcam library gcastartup
+10:59:28 D focusValue: 10
+10:59:28 W getViewfinderFormat 256
+10:59:28 I StreamConfigurationMap: 3; SDK: 34
+10:59:28 W getImageReaderFormat ImageReaderFormat{ImageFormat=RAW10, Size=4000x3000}
+10:59:28 I StreamConfigurationMap: 5; SDK: 34
+10:59:28 I App returns to the foreground
+10:59:28 I StreamConfigurationMap: 6; SDK: 34
+10:59:28 I Device Interface CameraID:0 9 => raven => flame
+10:59:28 D getStreamConfig sMode:0 key:pref_opmode_photo_key value:0
+10:59:28 I StreamConfigurationMap: 1; SDK: 34
+10:59:28 D getStreamMode 0
+10:59:28 D Hardware Level: LEVEL_3(3) => LEVEL_3(3)
+10:59:28 I StreamConfigurationMap: 4; SDK: 34
+10:59:28 I StreamConfigurationMap: 2; SDK: 34
+10:59:28 I Device Interface CameraID:0 9 => raven => flame
+10:59:28 W 
+10:59:28 W MetadataConverter CameraId: 0
+10:59:28 D SENSOR_INFO_PHYSICAL_SIZE >= width:7.68 height:5.76
+10:59:28 I HDR+ Model CameraID:0 raven => bramble
+10:59:28 W MetadataConverter SensorId: 0
+10:59:28 W MetadataConverter end
+10:59:28 W 
+10:59:28 W MetadataConverter CameraId: 1
+10:59:28 D SENSOR_INFO_PHYSICAL_SIZE >= width:3.65568 height:2.74176
+10:59:28 I HDR+ Model CameraID:0 raven => bramble
+10:59:28 W MetadataConverter SensorId: 1
+10:59:28 W MetadataConverter end
+10:59:28 I StreamConfigurationMap: 0; SDK: 34
+10:59:28 D HdrPlusSession Created
+10:59:28 W Focus-Tracking in Photo/Video mode true false
+10:59:28 W microVideo width 1280
+10:59:28 W microVideo height 720
+10:59:28 W Focus-Tracking in Photo/Video mode true false
+10:59:28 D getPortraitZoomValue 1.5 1.5
+10:59:28 E ZoomUI onProgressChanged 0
+10:59:28 E ZoomUI onProgressChanged 1
+10:59:28 E setSMode PHOTO
+10:59:28 D Switch Front false
+10:59:28 E ZoomUI onProgressChanged 0
+10:59:28 D JPGQuality ikg->a() 100
+10:59:29 D setCameraCaptureSession: 0
+10:59:29 D setCameraCaptureSession: 0
+10:59:29 D setCameraCaptureSession: 0
+10:59:31 D JPGQuality bgm->clinit() 100
+10:59:31 D JPGQuality ikg->a() 100
+10:59:32 I StreamConfigurationMap: 7; SDK: 34
+10:59:32 W startShotCaptureInternal
+10:59:32 D new ShotParams
+10:59:32 D setup
+10:59:32 D createAeShotParams
+10:59:32 D portraitRelighting
+10:59:33 D profile
+10:59:33 D DCI-P3 true
+10:59:33 D flash
+10:59:33 D tempBinning: true
+10:59:33 D wbSource
+10:59:33 D sabre
+10:59:33 D DevSettings 0
+10:59:33 D DevSettings -> (merge 0) -> wienerForce false
+10:59:33 D DevSettings -> (merge 1) -> sabreAllowed true
+10:59:33 D DevSettings -> (merge 2) -> Shasta true
+10:59:33 D DevSettings -> (merge 3) -> spatialRgbForce false
+10:59:33 D Merge Method 1
+10:59:33 D Allow Sabre true
+10:59:33 D Spatial RGB false
+10:59:33 D shasta
+10:59:33 D shastaZSL true
+10:59:33 D ShotParams_shasta_force_set false
+10:59:33 D setShasta rawFormat = RAW10(37)
+10:59:33 D finalize
+10:59:33 E getFrameCount pref_frame_count_key: -1=>12
+10:59:37 I StreamConfigurationMap: 7; SDK: 34
+10:59:37 W startShotCaptureInternal
+10:59:37 D new ShotParams
+10:59:37 D setup
+10:59:37 D createAeShotParams
+10:59:37 D portraitRelighting
+10:59:37 D profile
+10:59:37 D DCI-P3 true
+10:59:37 D flash
+10:59:37 D tempBinning: true
+10:59:37 D wbSource
+10:59:37 D sabre
+10:59:37 D DevSettings 0
+10:59:37 D DevSettings -> (merge 0) -> wienerForce false
+10:59:37 D DevSettings -> (merge 1) -> sabreAllowed true
+10:59:37 D DevSettings -> (merge 2) -> Shasta true
+10:59:37 D DevSettings -> (merge 3) -> spatialRgbForce false
+10:59:37 D Merge Method 1
+10:59:37 D Allow Sabre true
+10:59:37 D Spatial RGB false
+10:59:37 D shasta
+10:59:37 D shastaZSL true
+10:59:37 D ShotParams_shasta_force_set false
+10:59:37 D setShasta rawFormat = RAW10(37)
+10:59:37 D finalize
+10:59:37 E getFrameCount pref_frame_count_key: -1=>12
+10:59:37 D JPGQuality JpegUtilNative->a() 100
+10:59:40 D JPGQuality JpegUtilNative->a() 100
+10:59:41 D JPGQuality ikg->a() 100
+10:59:51 I App enter the background
